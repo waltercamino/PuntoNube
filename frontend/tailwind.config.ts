47f5a832795df07c71c2a483b70de4370088322e
Walter <106196739+waltercamino@users.noreply.github.com>
2025-09-20 10:30:07 -0300

    mover el package al front

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,11 +2,12 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+ content: [
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/**/*.{js,ts,jsx,tsx,mdx}',
+],
   theme: {
     extend: {
       backgroundImage: {
